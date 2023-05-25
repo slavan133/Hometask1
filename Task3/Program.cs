b05e1d0b5c9cfcea7046172540feed754e0fdf8c
Vyacheslav Nosov <slavan133@yandex.ru>
2023-05-25 18:46:21 +0300

    Задание 3 выполнено

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,16 +1,14 @@
-﻿// Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
-// a = 25, b=5, -> да
-// а = 2, b=10, -> нет
+﻿//Напишите программу, которая будет выдавать название дня недели по заданному номеру
+// 3-> среда
+// 5-> пятница
 
 Console.WriteLine("Введите число");
-int number1 = Convert.ToInt32(Console.ReadLine());
-int number2 = Convert.ToInt32(Console.ReadLine());
-int kvadrat = (number2 * number2);
-if (kvadrat == number1)
-{
-    Console.WriteLine("ДА");
-}
-else
-{
-    Console.WriteLine("НЕТ");
-}
+int numberofday = Convert.ToInt32(Console.ReadLine());
+if (numberofday == 1) Console.WriteLine("Monday");
+else if (numberofday == 2) Console.WriteLine("Tuesday");
+else if (numberofday == 3) Console.WriteLine("Wednesday");
+else if (numberofday == 4) Console.WriteLine("Thirsday");
+else if (numberofday == 5) Console.WriteLine("Friday");
+else if (numberofday == 6) Console.WriteLine("Saturday");
+else if (numberofday == 7) Console.WriteLine("Sunday");
+else Console.WriteLine("Нет такого номера дня недели");
